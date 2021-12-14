176a4d41295ecdd29d4f78baed1d5f01074b1101
Marcus Abukari <marcusabu@gmail.com>
2021-12-14 20:44:15 +0100

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,93 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [serverOutput, setServerOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function textIsValid(name) {
+    return name && name !== "";
+  }
+
+  function passwordIsValid(password) {
+    return password && password.length >= 8;
+  }
+
+  function resetForm() {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+    setServerOutput("");
+    setFormSubmitted(false);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setFormSubmitted(true);
+
+    if (formIsValid) {
+      resetForm();
+      console.log("Form submitted");
+      setIsLoading(true);
+
+      await axios
+        .post("https://demo-api.now.sh/users", {
+          firstName,
+          lastName,
+          email,
+          password,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch(() => {
+          setErrorMessage("Error uploading data");
+        });
+      await axios
+        .get(`https://demo-api.now.sh/users`)
+        .then((response) => {
+          console.log(response);
+          setServerOutput(response.data);
+        })
+        .catch(() => {
+          setErrorMessage("Error fetching data");
+        });
+
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setErrorMessage("");
+    setFormIsValid(true);
+
+    if (!textIsValid(firstName)) {
+      setFormIsValid(false);
+      setErrorMessage("First name is required");
+    }
+    if (!textIsValid(lastName)) {
+      setFormIsValid(false);
+      setErrorMessage("Last name is required");
+    }
+    if (!passwordIsValid(password)) {
+      setFormIsValid(false);
+      setErrorMessage("Password must meet the requirements");
+    }
+    if (!textIsValid(email)) {
+      setFormIsValid(false);
+      setErrorMessage("Email is required");
+    }
+  }, [firstName, lastName, email, password]);
+
   return (
     <div className="App">
       <div className="container">
@@ -8,11 +95,13 @@ function App() {
           <div className="col-md-6 offset-md-3">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">Sign Up</h5>
-                <form>
+                <h1 className="card-title">Sign Up</h1>
+                <form onSubmit={(e) => handleSubmit(e)}>
                   <div class="form-group">
                     <label for="firstnameInput">First name</label>
                     <input
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
                       type="text"
                       class="form-control"
                       id="firstnameInput"
@@ -22,6 +111,8 @@ function App() {
                   <div class="form-group">
                     <label for="lastnameInput">Last name</label>
                     <input
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
                       type="text"
                       class="form-control"
                       id="lastnameInput"
@@ -31,7 +122,9 @@ function App() {
                   <div class="form-group">
                     <label for="emailInput">Email address</label>
                     <input
-                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      type="text"
                       class="form-control"
                       id="emailInput"
                       placeholder="Enter email"
@@ -40,6 +133,8 @@ function App() {
                   <div class="form-group">
                     <label for="passwordInput">Password</label>
                     <input
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
                       type="password"
                       class="form-control"
                       id="passwordInput"
@@ -54,10 +149,24 @@ function App() {
                       </ul>
                     </small>
                   </div>
+                  {errorMessage && formSubmitted && (
+                    <div class="alert alert-danger" role="alert">
+                      {errorMessage}
+                    </div>
+                  )}
                   <button type="submit" class="btn btn-primary">
                     Send
                   </button>
                 </form>
+                {isLoading && (
+                  <div class="spinner-border text-dark" role="status"></div>
+                )}
+                {serverOutput && serverOutput !== "" && (
+                  <>
+                    <h2>Server output</h2>
+                    <code>{JSON.stringify(serverOutput)}</code>
+                  </>
+                )}
               </div>
             </div>
           </div>
