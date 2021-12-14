3fd13199e9c2ee98351c32a26e1841acadfe6314
Marcus Abukari <marcusabu@gmail.com>
2021-12-14 17:37:37 +0100

    Implement form skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Sign Up</h5>
+                <form>
+                  <div class="form-group">
+                    <label for="firstnameInput">First name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="firstnameInput"
+                      placeholder="First name"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="lastnameInput">Last name</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="lastnameInput"
+                      placeholder="Last name"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="emailInput">Email address</label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="emailInput"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="passwordInput">Password</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="passwordInput"
+                      placeholder="Password"
+                    />
+                    <small id="passwordInput" class="form-text text-muted">
+                      Requirements:
+                      <ul>
+                        <li>Should be a minimum of 8 characters</li>
+                        <li>Should contain lower and uppercase letters</li>
+                        <li>Should not contain your first or last name</li>
+                      </ul>
+                    </small>
+                  </div>
+                  <button type="submit" class="btn btn-primary">
+                    Send
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
